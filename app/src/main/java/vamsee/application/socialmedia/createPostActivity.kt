3368cb1428e467ba04a44eefb86a49bc8c16f17e
vamseevk <vamseevk2001@gmail.cmo>
2021-04-11 17:20:36 +0530

    user can selct the image from device and cn preview it in the create post activity

--- app/src/main/java/vamsee/application/socialmedia/createPostActivity.kt
+++ app/src/main/java/vamsee/application/socialmedia/createPostActivity.kt
@@ -1,11 +1,24 @@
 package vamsee.application.socialmedia
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_create_post.*
 import vamsee.application.socialmedia.daos.PostDao
+import java.io.IOException
 
 class createPostActivity : AppCompatActivity() {
+
+    private val PICK_IMAGE_REQUEST = 71
+    private var filePath: Uri? = null
+    private var firebaseStore: FirebaseStorage? = null
+    private var storageReference: StorageReference? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
@@ -13,6 +26,39 @@ class createPostActivity : AppCompatActivity() {
         post.setOnClickListener{
             post_text()
             finish()
+        }
+
+        firebaseStore = FirebaseStorage.getInstance()
+        storageReference = FirebaseStorage.getInstance().reference
+
+        chooseImage.setOnClickListener{
+            launchGallery()
+        }
+
+    }
+
+    private fun launchGallery() {
+
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select picture"), PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            if(data == null || data.data == null){
+                return
+            }
+
+            filePath = data.data
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filePath)
+                image.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
         }
     }
 
@@ -22,6 +68,10 @@ class createPostActivity : AppCompatActivity() {
             val post = PostDao()
             post.addPost(input)
         }
+
+    }
+
+    private fun uploadImage(){
 
     }
 }
