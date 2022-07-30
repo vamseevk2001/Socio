62c0e10cc48ab8c2536b71eeda5bb6dba6ba9a84
vamseevk2001 <vamseevk2001@gmail.com>
2022-07-30 13:57:45 +0530

    added images in posts

--- app/src/main/java/vamsee/application/socialmedia/PostAdapter.kt
+++ app/src/main/java/vamsee/application/socialmedia/PostAdapter.kt
@@ -1,5 +1,6 @@
 package vamsee.application.socialmedia
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,12 +8,18 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.airbnb.lottie.LottieAnimationView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import vamsee.application.socialmedia.models.Post
+
+import com.bumptech.glide.request.target.Target
 
 class PostAdapter(options: FirestoreRecyclerOptions<Post >, val listener: IpostAdapter) : FirestoreRecyclerAdapter<Post, PostAdapter.PostViewHolder>(
     options) {
@@ -25,6 +32,7 @@ class PostAdapter(options: FirestoreRecyclerOptions<Post >, val listener: IpostA
         val userImage: ImageView = itemView.findViewById(R.id.userImage)
         val likeButton: ImageView = itemView.findViewById(R.id.likeButton)
         val postImg: ImageView = itemView.findViewById(R.id.post_img)
+        val loader: LottieAnimationView = itemView.findViewById(R.id.loading)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
@@ -42,7 +50,32 @@ class PostAdapter(options: FirestoreRecyclerOptions<Post >, val listener: IpostA
         holder.likeCount.text = model.likedBy.size.toString()
         Glide.with(holder.userImage.context).load(model.createdBy.imageId).circleCrop().into(holder.userImage)
         holder.createdAt.text = Utils.getTimeAgo(model.createdAt)
-        Glide.with(holder.postImg.context).load(model.imageUrl).into(holder.postImg)
+        Glide.with(holder.postImg.context).load(model.imageUrl).listener(object: RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                holder.loader.visibility = View.GONE
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                holder.loader.visibility = View.GONE
+                return false
+            }
+        }
+
+        )
+
+            .into(holder.postImg)
 
 
         val auth = Firebase.auth
